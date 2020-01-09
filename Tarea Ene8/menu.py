73740f89441d023b1f6b08c9f24660fd94bdc527
Juan Diego <jotade100@gmail.com>
2020-01-09 16:36:31 -0600

    Actualizado código según reglas Pep8
    Se utilizó la herramienta http://pep8online.com/checkresult para verificar que el código siguiese reglas PEP8

--- Tarea Ene8/menu.py
+++ Tarea Ene8/menu.py
@@ -1,8 +1,9 @@
 diccionarioOp = {
-    'suma': (lambda x, y: x+y), 
-    'resta': (lambda x, y: x-y), 
+    'suma': (lambda x, y: x + y),
+    'resta': (lambda x, y: x - y),
     'multiplicación': (lambda x, y: x*y)
     }
+
 
 def generarMenu(dic):
     """Generar el menú automáticamente:
@@ -10,13 +11,13 @@ def generarMenu(dic):
     print("Seleccione una opción: ")
     # Se guarda en forma de lista los elementos del diccionario
     enumeracion = list(enumerate(dic))
-    for x,y in enumeracion:
+    for x, y in enumeracion:
         print("\t {} ~ {}".format(x, y))
     seleccion = int(input("Introduzca una opción: "))
     if seleccion not in range(len(enumeracion)):
         print("Opción no válida")
-    else: 
+    else:
         resultado = dic.get(enumeracion[seleccion][1])
-        print("Resultado: {}".format( resultado(int(input("Introduzca el primer operando: ")), int(input("Introduzca el segundo operando: ")))))
+        print("Resultado: {}".format(resultado(int(input("Introduzca el primer operando: ")), int(input("Introduzca el segundo operando: ")))))
 
 generarMenu(diccionarioOp)
--- Tarea Ene8/tarea22.py
+++ Tarea Ene8/tarea22.py
@@ -5,10 +5,12 @@ import math
 # tuplas = [(-2, 9), (4, 6), (1, 0), (-5, 3)]
 tuplas = [(0, 1), (3, 5), (7, 2), (4, -2)]
 
+
 def distancia(w, z):
     """Calcular la distancia:
     Se determina la longitud del vector formado por dos puntos"""
     return math.sqrt(((w[0]-z[0])**2)+((w[1]-z[1])**2))
+
 
 def addIfNotInList(lista, elemento):
     """Ingreso excluyente:
@@ -16,15 +18,15 @@ def addIfNotInList(lista, elemento):
     if elemento not in lista:
         lista.append(elemento)
 
-# El método fue diseñado considerando que los vértices no son entregados de manera ordenada.
 
+# El método fue diseñado considerando que los vértices no son entregados de manera ordenada.
 def determinarSiEsCuadrado(tuplas):
     """Determina si 4 vértices conforman un cuadrado:
-    Calculando las distancias vectoriales entre distintas combinaciones de 
+    Calculando las distancias vectoriales entre distintas combinaciones de
     vértices se determina si es cuadrado"""
     rectas = []
     for x in range(3):
-        for y in range (x+1, 4):    
+        for y in range(x+1, 4):
             addIfNotInList(rectas, distancia(tuplas[x], tuplas[y]))
     # Si sólo hay dos elementos (lado y diagonal) es un cuadrado
     if len(rectas) != 2:
