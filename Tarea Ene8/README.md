760df6e90966f7361d52786b4911d3529ab43866
Juan Diego <jotade100@gmail.com>
2020-01-14 00:57:04 -0600

    Actualizada tarea 23
    Actualizada forma más eficiente de resolver problema 23.

--- Tarea Ene8/README.md
+++ Tarea Ene8/README.md
@@ -1,7 +1,7 @@
 # Tarea 8 de enero del 2020 AD
 La tarea consistía en:
 * Hacer un menú dinámico a partir de un diccionario
-* Resulver ejercicios 22 y 23
+* Resolver ejercicios 22 y 23
 
 ## Forma de entrega de tarea
 * Para el primer punto existe un archivo llamado menu.py
@@ -9,5 +9,5 @@ La tarea consistía en:
 
 ## Comentarios adicionales
 * Se incluyó el archivo de prueba de las instrucciones en triangle.txt
-* La tarea 23 cuenta con un borrador para comprender mejor de donde se partió hacia el resultado.
+* La tarea 23 cuenta con un borrador y una interpretación de grafo para comprender mejor de donde se partió hacia el resultado.
 * La tarea 22 fue solucionada de una manera no tan óptima debido a que los puntos no se entregan en orden.
--- Tarea Ene8/tarea23grafo.py
+++ Tarea Ene8/tarea23grafo.py
@@ -2,25 +2,27 @@ def leerArchivo(nombre):
     """Lectura de archivo:
     Lee un archivo y retorna su contenido separado por líneas en un arreglo
     y a su vez separado por palabras"""
-    return [list(map(int, line.split())) for line in open(nombre)]
+    return [line.rstrip('\n').split() for line in open(nombre)]
 
 
 def calcularCaminoAdyLargo(triangulo):
     """Cálculo de camino de adyacentes:
     Tras recorrer un triángulo rectángulo de datos retorna el camino de
     adyacentes que suma más. [Algoritmo propuesto por Ing. Samuel Chávez]
-    Su complejidad es de O(lado^2).
+    Su complejidad es de O(aristas + vertices) para el procesamiento.
     Su complejidad de almacenaje es el área del triángulo"""
-    cola = len(triangulo) - 1
-    while cola != 0:
-        cola -= 1
-        for w in range(0, cola + 1):
-            if triangulo[cola + 1][w] > triangulo[cola + 1][w + 1]:
-                triangulo[cola][w] += triangulo[cola + 1][w]
-            else:
-                triangulo[cola][w] += triangulo[cola + 1][w + 1]
-    return triangulo[0][0]
-
+    comodin = 0
+    cola = [(0, 0, int(triangulo[0][0]))]
+    while len(cola) != 0:
+        actual = cola.pop()
+        for w in range(actual[1], actual[1] + 2):
+            if ((actual[0]) == (len(triangulo) - 1)):
+                if comodin < actual[2]:
+                    comodin = actual[2]
+            if ((actual[0]) < (len(triangulo)-1)):
+                cola.append((actual[0] + 1, w,
+                            (actual[2] + int(triangulo[actual[0] + 1][w]))))
+    return comodin
 
 caminoMasLargo = calcularCaminoAdyLargo(leerArchivo("triangle.txt"))
 
