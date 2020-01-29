f422719744f7a10fe6e6440298132531e5b1df4e
Juan Diego <jotade100@gmail.com>
2020-01-29 10:31:25 -0600

    Agregada comprobación del ciclo
    Agregada la comprobación del ciclo de derivación

--- Descenso Gradiente/descensoVersion1.py
+++ Descenso Gradiente/descensoVersion1.py
@@ -1,33 +1,48 @@
 import numpy as np
+import random as rnd
 
 def derivada_teta_1(tetas, x, y, param):
     return (((np.dot(tetas, x) - y) * x[param]).sum()) / (len(x))
 
 
+def gen_data(n, bias, varianza):
+    x = []
+    y = []
+    for i in range(0, n):
+        x.append(i)
+        y.append((i + bias) + rnd.uniform(0, 1) * varianza)
+    return x, y
+
+
 # Datos iniciales
-tetas = [2.0, 1.0]
-x = [[1.0, 1.0], [4.0, 1.0]] # relleno la primera fila con 1 para el intercepto
-y = [9, 10]
+tetas = [1.0, 1.0]
+x, y = gen_data(50, 25, 50)
+x = [[1 for i in range(len(x))], x] # relleno la primera fila con 1 para el intercepto
 # Uso numpy para realizar operaciones con matrices y que sea más rápida
 #   la sumatoria de la función costo
 tetas = np.array(tetas)
-x = np.array(x)
+x = np.array(x, dtype='int64')
 y = np.array(y)
 # variables de flujo del gradiente
 razon_de_aprendizaje = 1
 teta_gradiente = tetas.copy()
 converger = True
-
+numero_de_iteraciones_maximo = 1000
+tolerancia = 1.5
 
 
 while (converger):
     # hacer derivada de funcion
     # moverse en el espacio
+    numero_de_iteraciones_maximo -= 1
     teta_temporal = teta_gradiente.copy()
     derivadas = []
     for i in range(len(teta_gradiente)):
         derivadas.append(derivada_teta_1(teta_temporal, x, y, i))
         teta_gradiente[i] -= razon_de_aprendizaje * derivadas[i]
+
+    if (abs(sum(derivadas) / len(derivadas)) < tolerancia) or (numero_de_iteraciones_maximo == 0):
+        converger = False
 
 
 
